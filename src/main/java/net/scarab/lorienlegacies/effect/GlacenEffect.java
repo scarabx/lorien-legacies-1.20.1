3be072c5c8fb7c082e0db14ad872874539da8e1b
Dassie123 <111228490+Dassie123@users.noreply.github.com>
2025-04-23 12:06:25 +0200

    Update GlacenEffect.java

--- src/main/java/net/scarab/lorienlegacies/effect/GlacenEffect.java
+++ src/main/java/net/scarab/lorienlegacies/effect/GlacenEffect.java
@@ -3,11 +3,9 @@ package net.scarab.lorienlegacies.effect;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
-import net.minecraft.entity.projectile.SmallFireballEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.Vec3d;
 import net.scarab.lorienlegacies.entity.IceballProjectileEntity;
 import net.scarab.lorienlegacies.item.ModItems;
 
