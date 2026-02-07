5b72fd5f33edcb712caf9ffe79ba1f9f4c9a2467
scarabx <111228490+scarabx@users.noreply.github.com>
2026-02-07 16:17:00 +0200

    Update RegenerasEffect.java

--- src/main/java/net/scarab/lorienlegacies/effect/passive_effects/RegenerasEffect.java
+++ src/main/java/net/scarab/lorienlegacies/effect/passive_effects/RegenerasEffect.java
@@ -6,7 +6,6 @@ import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
-import net.scarab.lorienlegacies.effect.ModEffects;
 
 import static net.scarab.lorienlegacies.effect.ModEffects.*;
 
@@ -19,20 +18,6 @@ public class RegenerasEffect extends StatusEffect {
     @Override
     public void applyUpdateEffect(LivingEntity entity, int amplifier) {
 
-        // Reapply invisibly if needed
-        StatusEffectInstance current = entity.getStatusEffect(this);
-        if (current != null && (current.shouldShowParticles() || current.shouldShowIcon())) {
-            entity.removeStatusEffect(this);
-            entity.addStatusEffect(new StatusEffectInstance(
-                    this,
-                    current.getDuration(),
-                    current.getAmplifier(),
-                    false,
-                    false,
-                    false
-            ));
-        }
-
         if (entity instanceof PlayerEntity player) {
             // Don't apply regeneration if the entity is tired
             if (player.hasStatusEffect(TIRED)
@@ -43,18 +28,17 @@ public class RegenerasEffect extends StatusEffect {
 
             // Apply regeneration if health is low
             if (!player.getWorld().isClient() && entity.getHealth() < entity.getMaxHealth()) {
-                //StatusEffectInstance regen = player.getStatusEffect(StatusEffects.REGENERATION);
-                //if (regen == null || regen.getAmplifier() < 4) {
+                StatusEffectInstance regen = player.getStatusEffect(StatusEffects.REGENERATION);
+                if (regen == null || regen.getAmplifier() < 4) {
                     player.addStatusEffect(new StatusEffectInstance(
                             StatusEffects.REGENERATION,
-                            //Integer.MAX_VALUE,
-                            1,
-                            0,
+                            200,
+                            4,
                             false,
                             false,
                             false
                     ));
-                //}
+                }
             } else if (player.getHealth() == player.getMaxHealth()) {
                 player.removeStatusEffect(StatusEffects.REGENERATION);
                 player.addStatusEffect(new StatusEffectInstance(REGENERAS_COOLDOWN, 200, 0, false, false, false));
